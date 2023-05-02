385af51e1ebc0eed89d58a207d06a462e8f430fa
Tran Van Vu <vu.tranvan@vti.com.vn>
2023-05-02 11:03:47 +0900

    'aaa;

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import UserInfo from './pages/UserInfo'
+import SignUp from './pages/SignUp'
+import PassWordReset from './pages/PassWordReset'
+import History from './pages/History'
+import ForgetPW from './pages/ForgetPW'
+
 function App() {
 
 
@@ -12,6 +18,11 @@ function App() {
       <Routes>
        <Route path='' element={<Home />} />
        <Route path='/login' element={<Login />} />
+       <Route path='/information' element={<UserInfo />} />
+       <Route path='/register' element={<SignUp />} />
+       <Route path='/resetpassword' element={<PassWordReset />} />
+       <Route path='/history' element={<History />} />
+       <Route path='/forgetPW' element={<ForgetPW />} />
      </Routes>
      </Router>
       </div>
---
+++ src/pages/PassWordReset.jsx
@@ -0,0 +1,8 @@
+function PassWordReset(){
+    return (
+        <div  className="text-red-200">
+            PassWordReset
+        </div>
+    )
+}
+export default PassWordReset
