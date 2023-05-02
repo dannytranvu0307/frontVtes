0d4c7a4b2da85f0988f6c0c5b61e4bb6fa88b4d5
Tran Van Vu <vu.tranvan@vti.com.vn>
2023-05-02 18:48:08 +0900

    feature login

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './i18n.js'
-import store from './reducer/store';
-import { Provider } from 'react-redux';
+import store from './reducer/store'
+import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
