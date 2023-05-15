cb9e399f1753fd877e0f0cbe79022d2d9a76dc7c
Tran Van Vu <vu.tranvan@vti.com.vn>
2023-05-15 17:42:27 +0900

    ff

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,54 @@
+import { useState, useEffect, useCallback } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import Aos from "aos";
+import 'aos/dist/aos.css';
 
 import Navbar from './components/Navbar'
 import Language from './components/Language'
-
 
 import Home from './pages/Home';
 import Login from './pages/Login';
 import UserInfo from './pages/UserInfo';
 import SignUp from './pages/SignUp';
-import PasswordReset from './pages/PasswordReset';
+import PassWordReset from './pages/PassWordReset';
 import History from './pages/History';
 import ForgetPW from './pages/ForgetPW';
 import Demo from './pages/Demo';
 import ReduxLearning from './pages/ReduxLearning';
-import ConfirmResetPassword from './pages/ConfirmResetPassword';
 
 function App() {
+  const {t} = useTranslation();
+  useEffect(function () {
+    Aos.init({ duration: 3000 });
+  }, []);
 
   return (
+    <>
     <Router> 
       <header className="sticky top-0">
-          <Navbar>
+          <Navbar t={t} >
             <Language />
           </Navbar>
       </header>
+
       <main>
-        <Routes>
-          <Route path='' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/information' element={<UserInfo />} />
-          <Route path='/register' element={<SignUp />} />
-          <Route path='/history' element={<History />} />
-          <Route path='/forgetPW' element={<ForgetPW />} />
-          <Route path='/demo' element={<Demo />} />
-          <Route path='/a' element={<ReduxLearning />} />
-          <Route path='/resetpassword' element={<PasswordReset />} />
-          <Route path='/confirmresetpassword' element={<ConfirmResetPassword/>}></Route>
-        </Routes>
+            
+            <Routes>
+            <Route path='' element={<Home />} />
+            <Route path='/login' element={<Login t={t}/>} />
+            <Route path='/information' element={<UserInfo />} />
+            <Route path='/register' element={<SignUp />} />
+            <Route path='/resetpassword' element={<PassWordReset />} />
+            <Route path='/history' element={<History />} />
+            <Route path='/forgetPW' element={<ForgetPW />} />
+            <Route path='/demo' element={<Demo />} />
+            <Route path='/a' element={<ReduxLearning />} />
+          </Routes>
+        
       </main>
-    </Router>
+      </Router>
+    </>
   )
 }
 
